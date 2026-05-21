5e0a029ef77c2eebdb5cadcd1a5dbd313b1df310
Principia4834 <3509988+Principia4834@users.noreply.github.com>
2026-05-21 11:09:46 +0100

    Fix installation projects - wix project files - local publish files

---
+++ BuildRelease.ps1
@@ -0,0 +1,9 @@
+﻿# 1. Publish the app for win-x64 first
+dotnet publish GreenSwamp.Alpaca.Server/GreenSwamp.Alpaca.Server.csproj -c Release -r win-x64 --self-contained true -o publish/win-x64
+
+# 2. Build the MSI (x64)
+dotnet build Installer/Windows/GreenSwamp.Alpaca.Installer.wixproj -c Release -p:Platform=win-x64 -p:UpgradeCode="0BFB5E9F-4475-4BFA-A8A5-1F26CD982B1C" ` "-p:PublishDir=$PWD/publish/win-x64/"
+
+# 3. The MSI will be at:
+#    Installer\Windows\bin\win-x64\Release\GreenSwamp.Alpaca.Server_<version>_win-x64.msi
+#    Install it and verify the service starts and the Start Menu shortcut opens localhost:31426
